2c4c19b281a01c528cef91b1576d5a5e4bc3261f
Luke Todd <luketodd@zoho.com>
2021-12-12 19:05:04 +0000

    Started substrate kitties and now on the dispatchables, events and errors section that needs starting

---
+++ kitties/pallets/kitties/src/lib.rs
@@ -0,0 +1,138 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use pallet::*;
+
+#[frame_support::pallet]
+pub mod pallet {
+    use frame_support::pallet_prelude::*;
+    use frame_system::pallet_prelude::*;
+    use frame_support::{
+        sp_runtime::traits::Hash,
+        traits::{ Randomness, Currency, tokens::ExistenceRequirement },
+        transactional
+    };
+    use sp_io::hashing::blake2_128;
+    use scale_info::TypeInfo;
+
+    #[cfg(feature = "std")]
+    use frame_support::serde::{Deserialize, Serialize};
+
+	type AccountOf<T> = <T as frame_system::Config>::AccountId;
+    type BalanceOf<T> = <<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
+
+    #[derive(Clone, Encode, Decode, PartialEq, RuntimeDebug, TypeInfo)]
+    #[scale_info(skip_type_params(T))]
+    pub struct Kitty<T: Config> {
+        pub dna: [u8; 16],
+        pub price: Option<BalanceOf<T>>,
+        pub gender: Gender,
+        pub owner: AccountOf<T>,
+    }
+
+    #[derive(Clone, Encode, Decode, PartialEq, RuntimeDebug, TypeInfo)]
+    #[scale_info(skip_type_params(T))]
+    #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+    pub enum Gender {
+        Male,
+        Female
+    }
+
+    // ACTION #3: Implementation to handle Gender type in Kitty struct.
+
+    #[pallet::pallet]
+    #[pallet::generate_store(pub(super) trait Store)]
+    pub struct Pallet<T>(_);
+
+    /// Configure the pallet by specifying the parameters and types it depends on.
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        /// Because this pallet emits events, it depends on the runtime's definition of an event.
+        type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+
+        /// The Currency handler for the Kitties pallet.
+        type Currency: Currency<Self::AccountId>;
+
+        type KittyRandomness: Randomness<Self::Hash, Self::BlockNumber>;
+
+        type MaxKittyOwned: Get<u32>;
+    }
+
+    // Errors.
+    #[pallet::error]
+    pub enum Error<T> {
+        // TODO Part III
+    }
+
+    // Events.
+    #[pallet::event]
+    #[pallet::generate_deposit(pub(super) fn deposit_event)]
+    pub enum Event<T: Config> {
+        // TODO Part III
+    }
+
+    #[pallet::storage]
+    #[pallet::getter(fn all_kitties_count)]
+    pub(super) type KittyCnt<T: Config> = StorageValue<_, u64, ValueQuery>;
+
+    #[pallet::storage]
+    #[pallet::getter(fn kitties)]
+    pub(super) type Kitties<T: Config> = StorageMap<
+        _,
+        Twox64Concat,
+        T::Hash,
+        Kitty<T>,
+    >;
+
+    #[pallet::storage]
+    #[pallet::getter(fn kitties_owned)]
+    pub(super) type KittiesOwned<T: Config> = StorageMap<
+        _,
+        Twox64Concat,
+        T::AccountId,
+        BoundedVec<T::Hash, T::MaxKittyOwned>,
+        ValueQuery,
+    >;
+
+    // TODO Part IV: Our pallet's genesis configuration.
+
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+
+        // TODO Part III: create_kitty
+
+        // TODO Part IV: set_price
+
+        // TODO Part IV: transfer
+
+        // TODO Part IV: buy_kitty
+
+        // TODO Part IV: breed_kitty
+    }
+
+    //** Our helper functions.**//
+
+    impl<T: Config> Pallet<T> {
+
+        fn gen_gender() -> Gender {
+            let random = T::KittyRandomness::random(&b"gender"[..]).0;
+            match random.as_ref()[0] % 2 {
+                0 => Gender::Male,
+                _ => Gender::Female,
+            }
+        }
+
+        // TODO Part III: helper functions for dispatchable functions
+
+        fn gen_dna() -> [u8; 16] {
+            let payload = (
+                T::KittyRandomness::random(&b"dna"[..]).0,
+                <frame_system::Pallet<T>>::block_number(),
+            );
+            payload.using_encoded(blake2_128)
+        }
+
+        // TODO Part III: mint
+
+        // TODO Part IV: transfer_kitty_to
+    }
+}
